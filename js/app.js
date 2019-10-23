373d82975f93bcda2a1eb6972fffb5e8cc81eb24
CHENXI GUO <u6695264@anu.edu.au>
2019-10-24 03:35:17 +1100

    format js

--- js/app.js
+++ js/app.js
@@ -2,174 +2,100 @@ let mainNav = document.getElementById('menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
 navBarToggle.addEventListener('click', function () {
-  mainNav.classList.toggle('active');
+    mainNav
+        .classList
+        .toggle('active');
 });
 
-
-// var title = document.getElementsByTagName("title")[0].innerHTML;
-// document.getElementById("artistName").innerHTML = title;
-// console.log(title);
-
 var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-
-// var url = `https://newsapi.org/v2/everything?` +
-//           `q=Artwork&` +
-//           `from=${date}` +
-//           `sortBy=popularity&` +
-//           `apiKey=9ec9cdd0f4bf4ca1b3e00913ee10f819`;
-
-// var req = new Request(url);
-
-// fetch(req)
-//     .then(function(response) {
-//       return response.json();
-//     })
-//     .then(function(res){
-//       console.log(res.articles);
-//     })
+var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 
 function initMap() {
-  var uluru = {
-    lat: -35.280591,
-    lng: 149.122572
-  };
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 16,
-    center: uluru
-  });
+    var uluru = {
+        lat: -35.280591,
+        lng: 149.122572
+    };
+    var map = new google
+        .maps
+        .Map(document.getElementById('map'), {
+            zoom: 16,
+            center: uluru
+        });
 
-  var contentString = '<div class="iw-container">' +
-    '<div class="iw-title">ANU School of Art & Design</div>' +
-    '<div class="iw-content">' +
+    var contentString = '<div class="iw-container"><div class="iw-title">ANU School of Art & Design</div>' +
+            '<div class="iw-content"><span class="fa fa-map-marker" aria-hidden="true"> </spa' +
+            'n> <p>Address: 105 Childers St, Acton ACT 2601 </p><br><span class="fa fa-globe"' +
+            '" aria-hidden="true"> </span> <p>Website: </p><a href="soad.cass.anu.edu.au">soa' +
+            'd.cass.anu.edu.au</a><br><span class="fa fa-phone"" aria-hidden="true"> </span> ' +
+            '<p>Phone: (02) 6125 5810</p><br><span class="fa fa-star"" aria-hidden="true"> </' +
+            'span> <p>Rating: 4.7</p><br><span class="fa fa-clock-o"" aria-hidden="true"> </s' +
+            'pan> <p>opening hours:</p><br><p>weekdays: 2-4:30pm</p><br><p>weekends: closed</' +
+            'p></div></div>';
 
-    '<span class="fa fa-map-marker" aria-hidden="true"> </span> <p>Address: 105 Childers St, Acton ACT 2601 </p>' +
-    '<br>' +
-    '<span class="fa fa-globe"" aria-hidden="true"> </span> <p>Website: </p><a href="soad.cass.anu.edu.au">soad.cass.anu.edu.au</a>' +
+    var infowindow = new google
+        .maps
+        .InfoWindow({content: contentString});
 
-    '<br>' +
-    '<span class="fa fa-phone"" aria-hidden="true"> </span> <p>Phone: (02) 6125 5810</p>' +
-
-    '<br>' +
-    '<span class="fa fa-star"" aria-hidden="true"> </span> <p>Rating: 4.7</p>' +
-
-    '<br>' +
-    '<span class="fa fa-clock-o"" aria-hidden="true"> </span> <p>opening hours:</p>' +
-    '<br>' +
-    '<p>weekdays: 2-4:30pm</p>' +
-    '<br>' +
-    '<p>weekends: closed</p>' +
-    '</div>' +
-    '</div>';
-
-
-  var infowindow = new google.maps.InfoWindow({
-    content: contentString
-  });
-
-  var marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-    title: 'ANU graudate show'
-  });
-  marker.addListener('click', function () {
+    var marker = new google
+        .maps
+        .Marker({position: uluru, map: map, title: 'ANU graudate show'});
+    marker.addListener('click', function () {
+        infowindow.open(map, marker);
+    });
     infowindow.open(map, marker);
-  });
-  infowindow.open(map, marker);
 
-  var trafficLayer = new google.maps.TrafficLayer();
-  trafficLayer.setMap(map);
+    var trafficLayer = new google
+        .maps
+        .TrafficLayer();
+    trafficLayer.setMap(map);
 
-  var transitLayer = new google.maps.TransitLayer();
-  transitLayer.setMap(map);
+    var transitLayer = new google
+        .maps
+        .TransitLayer();
+    transitLayer.setMap(map);
 }
-
 
 var artist_url = "../data/artist.json";
 fetch(artist_url).then(function (res) {
-  return res.json();
+    return res.json();
 })
-.then(function (data) {
-  console.log(data);
-  
+    .then(function (data) {
+        console.log(data);
 
-});
-
+    });
 
 var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides((slideIndex += n));
+    showSlides((slideIndex += n));
 }
 
 function currentSlide(n) {
-  showSlides((slideIndex = n));
+    showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slider");
-  console.log(slides.length);
-  var dots = document.getElementById("demo");
+    var i;
+    var slides = document.getElementsByClassName("slider");
+    console.log(slides.length);
+    var dots = document.getElementById("demo");
 
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-    
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i]
-      .className
-      .replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i]
+            .className
+            .replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
 
 }
-
-
-
-
-
-
-//recursive method to check the width for the lacking hover effect on mobile view;
-
-
-
-
-
-
-
-// var slideIndex = 1;
-// showSlides(slideIndex);
-
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("demo");
-//   var captionText = document.getElementById("caption");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-//   captionText.innerHTML = dots[slideIndex-1].alt;
-// }
--- js/artist.js
+++ js/artist.js
@@ -2,9 +2,9 @@ let mainNav = document.getElementById('menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
 navBarToggle.addEventListener('click', function () {
-  mainNav
-    .classList
-    .toggle('active');
+    mainNav
+        .classList
+        .toggle('active');
 });
 
 var today = new Date();
@@ -16,100 +16,92 @@ var req = new Request(url);
 
 fetch(req).then(function (response) {
     return response.json();
-  })
-  .then(function (res) {
-    console.log(res.articles);
-  })
+})
+    .then(function (res) {
+        console.log(res.articles);
+    })
 
 function initMap() {
-  var uluru = {
-    lat: -35.280591,
-    lng: 149.122572
-  };
-  var map = new google
-    .maps
-    .Map(document.getElementById('map'), {
-      zoom: 16,
-      center: uluru
-    });
+    var uluru = {
+        lat: -35.280591,
+        lng: 149.122572
+    };
+    var map = new google
+        .maps
+        .Map(document.getElementById('map'), {
+            zoom: 16,
+            center: uluru
+        });
 
-  var contentString = '<div class="iw-container"><div class="iw-title">ANU School of Art & Design</div>' +
-    '<div class="iw-content"><span class="fa fa-map-marker" aria-hidden="true"> </spa' +
-    'n> <p>Address: 105 Childers St, Acton ACT 2601 </p><br><span class="fa fa-globe"' +
-    '" aria-hidden="true"> </span> <p>Website: </p><a href="soad.cass.anu.edu.au">soa' +
-    'd.cass.anu.edu.au</a><br><span class="fa fa-phone"" aria-hidden="true"> </span> ' +
-    '<p>Phone: (02) 6125 5810</p><br><span class="fa fa-star"" aria-hidden="true"> </' +
-    'span> <p>Rating: 4.7</p><br><span class="fa fa-clock-o"" aria-hidden="true"> </s' +
-    'pan> <p>opening hours:</p><br><p>weekdays: 2-4:30pm</p><br><p>weekends: closed</' +
-    'p></div></div>';
+    var contentString = '<div class="iw-container"><div class="iw-title">ANU School of Art & Design</div>' +
+            '<div class="iw-content"><span class="fa fa-map-marker" aria-hidden="true"> </spa' +
+            'n> <p>Address: 105 Childers St, Acton ACT 2601 </p><br><span class="fa fa-globe"' +
+            '" aria-hidden="true"> </span> <p>Website: </p><a href="soad.cass.anu.edu.au">soa' +
+            'd.cass.anu.edu.au</a><br><span class="fa fa-phone"" aria-hidden="true"> </span> ' +
+            '<p>Phone: (02) 6125 5810</p><br><span class="fa fa-star"" aria-hidden="true"> </' +
+            'span> <p>Rating: 4.7</p><br><span class="fa fa-clock-o"" aria-hidden="true"> </s' +
+            'pan> <p>opening hours:</p><br><p>weekdays: 2-4:30pm</p><br><p>weekends: closed</' +
+            'p></div></div>';
 
-  var infowindow = new google
-    .maps
-    .InfoWindow({
-      content: contentString
-    });
+    var infowindow = new google
+        .maps
+        .InfoWindow({content: contentString});
 
-  var marker = new google
-    .maps
-    .Marker({
-      position: uluru,
-      map: map,
-      title: 'ANU graudate show'
+    var marker = new google
+        .maps
+        .Marker({position: uluru, map: map, title: 'ANU graudate show'});
+    marker.addListener('click', function () {
+        infowindow.open(map, marker);
     });
-  marker.addListener('click', function () {
     infowindow.open(map, marker);
-  });
-  infowindow.open(map, marker);
 
-  var trafficLayer = new google
-    .maps
-    .TrafficLayer();
-  trafficLayer.setMap(map);
+    var trafficLayer = new google
+        .maps
+        .TrafficLayer();
+    trafficLayer.setMap(map);
 
-  var transitLayer = new google
-    .maps
-    .TransitLayer();
-  transitLayer.setMap(map);
+    var transitLayer = new google
+        .maps
+        .TransitLayer();
+    transitLayer.setMap(map);
 }
 
 function validateImage(url) {
-  if (url == "") {
-    url = "NoImageFound.jpg";
-    return url;
-  } else {
-    return url;
-  }
+    if (url == "") {
+        url = "NoImageFound.jpg";
+        return url;
+    } else {
+        return url;
+    }
 }
 
-
-function goToBottom(){
-  window.location.href = "#map";
+function goToBottom() {
+    window.location.href = "#map";
 }
 
 var artist_url = "../data/artist.json";
 
 fetch(artist_url).then(function (res) {
     return res.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    console.log(data.length);
+})
+    .then(function (data) {
+        console.log(data);
+        console.log(data.length);
 
-    var i;
+        var i;
 
-    for (i = 0; i < data.length; i++) {
-      console.log("test");
+        for (i = 0; i < data.length; i++) {
+            console.log("test");
 
-      var newElement = document.createElement('div');
-      newElement.setAttribute("id", "each-artist");
+            var newElement = document.createElement('div');
+            newElement.setAttribute("id", "each-artist");
 
-      newElement.innerHTML = `<div class= "artist-design"style="display: inline-block; border: 10px solid pink; border-image-source: url(../images/border.png); border-image-slice: 20 22;border-image-repeat: round; width: 300px;height: 200px; background-image: url(../images/thumbnails/${validateImage(data[i].Thumbnail)
+            newElement.innerHTML = `<div class= "artist-design"style="display: inline-block; border: 10px solid pink; border-image-source: url(../images/border.png); border-image-slice: 20 22;border-image-repeat: round; width: 300px;height: 200px; background-image: url(../images/thumbnails/${validateImage(data[i].Thumbnail)
                 .split(' ')
                 .join('%20')}); background-size: auto 100%; background-position: center center; background-repeat: no-repeat; margin: 0"></div>` + `<h1 class="artist-name">${data[i].First + ' ' + data[i].Last} </h1>` + `<div class="artist-Degree-Container">` + ` <i class="fa fa-university" aria-hidden="true"></i>` + `<div class="artist-Degree">${data[i].degree}</div>` + '</div>' + `<div class="artist-Workshop">${data[i].workshop}</div>` + `<div class="artist-Social-Container">` + `<div><a href=${data[i].Website}><i class="fa fa-globe" aria-hidden="true"></i></a></div>` + `<div><a href=${data[i].Social}><i class="fa fa-share-square-o" aria-hidden="true"></i></a></div>` + `</div>`;
-      document
-        .getElementById("artist-list-container")
-        .appendChild(newElement);
-    }
+            document
+                .getElementById("artist-list-container")
+                .appendChild(newElement);
+        }
 
-  });
-
+    });
